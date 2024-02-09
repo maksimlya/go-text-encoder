48169f1f4b8acfddc6c8830001ff04a4f7fe8b3d
Maksim L <maksimlya@gmail.com>
2024-02-09 13:33:31 +0200

    Updates - add decode.

--- binding/main.go
+++ binding/main.go
@@ -7,58 +7,35 @@ package main
 typedef struct  { void* message; int size; char* error; } GoResponse;
 
 GoResponse* Encode(char*);
+char* Decode(void* ptr, int length);
 
-__attribute__((weak))
-int myFunction() {
-
-	char arr[4] = "mama";
-	void* z = (void *)(intptr_t)Encode(arr);
-	uint8_t *boss = z;
-	printf("%d", boss[1]);
-	printf("\n");
-	return 1;
-}
-
-__attribute__((weak))
-void* getArray() {
-	uint8_t* boss = (uint8_t*)malloc(4 * sizeof(uint8_t));
-    boss[0] = 1;
-    boss[1] = 2;
-    boss[2] = 3;
-    boss[3] = 4;
-
-	return boss;
-}
 */
 import "C"
 import (
 	"encoder"
+	"unsafe"
 )
 
 //export Encode
 func Encode(message *C.char) *C.GoResponse {
 	encoded := encoder.Encode(C.GoString(message))
-	result := (*C.GoResponse)(C.malloc(C.size_t(C.sizeof_GoResponse)))
+    result := (*C.GoResponse)(C.malloc(C.size_t(C.sizeof_GoResponse)))
 	result.error = nil
 	result.message = C.CBytes(encoded)
 	result.size = C.int(len(encoded))
+
+	// Memory is freed on caller side
 	return result
 }
+
 //export Decode
-func Decode(encoded []byte) string {
-	return encoder.Decode(encoded)
+func Decode(ptr unsafe.Pointer, length C.int) *C.char {
+	data := C.GoBytes(ptr, length)
+    result := encoder.Decode(data)
+	decodedCString := C.CString(result)
+
+	// Memory is freed on caller side
+	return decodedCString
 }
 
-func main() {
-	C.myFunction();
-	// gg := C.getArray()
-
-	// // Convert the C array to a Go slice for easier manipulation
-	// arraySize := 4
-	// array := (*[1 << 30]C.uint8_t)(gg)[:arraySize:arraySize]
-
-	// // Access the elements of the array
-	// for i, value := range array {
-	// 	fmt.Printf("Element %d: %v\n", i, value)
-	// }
-}
+func main() {}
